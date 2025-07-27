42d7b0a13768db4729a3f95848f93a28f4f06bc6
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-07-27 02:09:46 -0300

    fix: eslint.config.mjs

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,7 +10,7 @@ const RecentProjects = () => {
     return (
         <div className="py-20" id="projects">
             <h1 className="heading">
-                Uma pequena seleção de{" "}
+                Uma pequena seção de{" "}
                 <span className="text-purple">projetos recentes</span>
             </h1>
             <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
